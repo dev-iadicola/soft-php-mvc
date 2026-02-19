ad77c7a53f4343a75dd7c92f283331a2a163849b
Luigi Iadicola <luigiadicola@gmail.com>
2026-02-19 11:46:15 +0100

    refactor(controller): Format `ProgettiController` for PSR-12 compliance
    Adjusted class, methods, and code structure to conform to PSR-12 coding standards for improved readability and consistency.

--- App/Controllers/ProgettiController.php
+++ App/Controllers/ProgettiController.php
@@ -1,28 +1,31 @@
 <?php
+
 namespace App\Controllers;
+
 use App\Model\Project;
 use App\Core\Controllers\Controller;
 use App\Core\Http\Attributes\RouteAttr;
 use App\Core\Http\Request;
 
-class ProgettiController extends Controller {
-
-  
+class ProgettiController extends Controller
+{
     #[RouteAttr('progetti')]
-    public function index() {
-     $projects = Project::query()->all();
+    public function index(): void
+    {
+        $projects = Project::query()->all();
 
-      view('progetti', compact('projects'));
+        view('progetti', compact('projects'));
     }
 
     #[RouteAttr('progetti/{slug}')]
-    public function show(Request $request, string $slug): void{
-         $project = is_numeric($slug)
-             ? Project::query()->find((int) $slug)
-             : Project::query()->where('title', $slug)->first();
-         $projects = Project::query()->all();
-         view('progetto', compact('project', "projects"));
+    public function show(Request $request, string $slug): void
+    {
+        $project = is_numeric($slug)
+            ? Project::query()->find((int) $slug)
+            : Project::query()->where('title', $slug)->first();
+
+        $projects = Project::query()->all();
+
+        view('progetto', compact('project', 'projects'));
     }
-
-
 }
